0f670589e12fb97ae7cbeec85d805f317ad81855
LovaPinto <lovasoa.alexis@gmail.com>
2025-06-29 11:58:18 +0300

    modification sur la fonction selectdeprtemnt

--- page/assert/traitement/function.php
+++ page/assert/traitement/function.php
@@ -1,12 +1,10 @@
 <?php
 function selectDepartement($db){
     $sql=sprintf("SELECT departments.*,employees.first_name,employees.last_name FROM `departments`
-        JOIN dept_emp
-        ON dept_emp.dept_no=departments.dept_no
         JOIN dept_manager
-        ON dept_manager.emp_no=dept_emp.emp_no
+        ON dept_manager.dept_no=departments.dept_no
         JOIN employees
-        on employees.emp_no=dept_manager.emp_no");
+        on employees.emp_no=dept_manager.emp_no order by dept_no desc");
 return $statement=mysqli_query($db,$sql);
 }
 ?>
--- page/index.php
+++ page/index.php
@@ -1,30 +1,44 @@
+<?php
+include(__DIR__ . "/assert/traitement/function.php");
+include(__DIR__ . "/assert/connectionBD.php");
+
+$db=connectionBD();
+$departement=selectDepartement($db);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
-    <script src="/bootstrap/js/bootstrap.bundle.min.js" defer></script>
+    <link rel="stylesheet" href="/page/bootstrap/css/bootstrap.min.css">
+    <script src="/page/bootstrap/js/bootstrap.bundle.min.js" defer></script>
     <title>Document</title>
 </head>
 <body>
     
-<table class="table">
+<table class="table table-striped-columns">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">dept_no</th>
+      <th scope="col">departement</th>
+      <th scope="col">first name manager</th>
+      <th scope="col">last name</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
+      <?php
+          while($donne=mysqli_fetch_assoc($departement)){
+            echo'  <tr>
+                      <th scope="row">'.$donne["dept_no"].'</th>
+                      <td>'.$donne["dept_name"].'</td>
+                      <td>'.$donne["first_name"].'</td>
+                      <td>'.$donne["last_name"].'</td>
+                  </tr>';
+          }
+      
+      ?>
   </tbody>
 </table>
 </body>
